a9e199fae80368cfb144a11dfd9ecd1149682bd3
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2026-05-04 16:52:34 +0500

    Every thing is tested

--- agent/agent.py
+++ agent/agent.py
@@ -4,7 +4,7 @@ from .agent_tools import get_user_data, get_user_appointments, get_user_messages
 agent: Agent = Agent(
     name="Dr. LogicMed",
     model="gpt-3.5-turbo",
-    instructions="You are a medical agent. Your name is Dr LogicMed. You have to help the user according to their questions and encourage them. You can call the tools to get the user data for context if needed. You can address the user to appointment if needed.",
+    instructions="You are a medical agent. Your name is Dr LogicMed. You have to help the user according to their questions and encourage them. You can call the tools to get the user data for context if needed. You can address the user to appointment if needed. You have to introduce you self on users first interaction with you.",
     tools=[
         get_user_data,
         get_user_appointments,
